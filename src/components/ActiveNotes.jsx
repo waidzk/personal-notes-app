1193fa09b86cf0bf902f55688f41c38c474d2a9d
Pradhityas Waidz Kamal <waidzk30@gmail.com>
2023-11-05 13:10:23 +0700

    feat: add info when there is no notes in archive or active

--- src/components/ActiveNotes.jsx
+++ src/components/ActiveNotes.jsx
@@ -6,33 +6,37 @@ function ActiveNotes({ notes, onArchiveHandler, onDeleteHandler }) {
     <div className="active-notes">
       <h2 className="title-note-section">Catatan Aktif</h2>
       <div className="notes">
-        {notes.map((note, i) => (
-          <div className="card" key={i}>
-            <div className="detail">
-              <h2 className="title-note">{note.title}</h2>
-              <span className="date-note">
-                {showFormattedDate(note.createdAt)}
-              </span>
-              <div className="body-note">
-                <p>{note.body}</p>
+        {notes.length !== 0 ? (
+          notes.map((note, i) => (
+            <div className="card" key={i}>
+              <div className="detail">
+                <h2 className="title-note">{note.title}</h2>
+                <span className="date-note">
+                  {showFormattedDate(note.createdAt)}
+                </span>
+                <div className="body-note">
+                  <p>{note.body}</p>
+                </div>
+              </div>
+              <div className="action-buttons">
+                <button
+                  className="btn-delete-note"
+                  onClick={() => onDeleteHandler(note.id)}
+                >
+                  Hapus
+                </button>
+                <button
+                  className="btn-archive-note"
+                  onClick={() => onArchiveHandler(note.id, note.archived)}
+                >
+                  Arsipkan
+                </button>
               </div>
             </div>
-            <div className="action-buttons">
-              <button
-                className="btn-delete-note"
-                onClick={() => onDeleteHandler(note.id)}
-              >
-                Hapus
-              </button>
-              <button
-                className="btn-archive-note"
-                onClick={() => onArchiveHandler(note.id, note.archived)}
-              >
-                Arsipkan
-              </button>
-            </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>Tidak ada catatan</p>
+        )}
       </div>
     </div>
   );
--- src/components/ArchiveNotes.jsx
+++ src/components/ArchiveNotes.jsx
@@ -6,33 +6,37 @@ function ArchiveNotes({ notes, onArchiveHandler }) {
     <div className="archive-notes">
       <h2 className="title-note-section">Arsip</h2>
       <div className="notes">
-        {notes.map((note, i) => (
-          <div className="card" key={i}>
-            <div className="detail">
-              <h2 className="title-note">{note.title}</h2>
-              <span className="date-note">
-                {showFormattedDate(note.createdAt)}
-              </span>
-              <div className="body-note">
-                <p>{note.body}</p>
+        {notes.length !== 0 ? (
+          notes.map((note, i) => (
+            <div className="card" key={i}>
+              <div className="detail">
+                <h2 className="title-note">{note.title}</h2>
+                <span className="date-note">
+                  {showFormattedDate(note.createdAt)}
+                </span>
+                <div className="body-note">
+                  <p>{note.body}</p>
+                </div>
+              </div>
+              <div className="action-buttons">
+                <button
+                  className="btn-delete-note"
+                  onClick={() => onDeleteHandler(note.id)}
+                >
+                  Hapus
+                </button>
+                <button
+                  className="btn-archive-note"
+                  onClick={() => onArchiveHandler(note.id, note.archived)}
+                >
+                  Aktifkan
+                </button>
               </div>
             </div>
-            <div className="action-buttons">
-              <button
-                className="btn-delete-note"
-                onClick={() => onDeleteHandler(note.id)}
-              >
-                Hapus
-              </button>
-              <button
-                className="btn-archive-note"
-                onClick={() => onArchiveHandler(note.id, note.archived)}
-              >
-                Aktifkan
-              </button>
-            </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>Tidak ada catatan</p>
+        )}
       </div>
     </div>
   );
